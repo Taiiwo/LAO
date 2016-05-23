e9bff3bed19d483277d1f8eb1ee7a36b1c5bbf6a
Taiiwo Llort <taiiwo@llort.gq>
2016-05-23 02:19:56 +0100

    add optional data param to get and post methods

--- requests.nim
+++ requests.nim
@@ -1,21 +1,42 @@
 import httpclient
-import tables
-import typetraits
 
 # define requests as a new type
 type Requests* = ref object of RootObj
+type dict = array[0..0, (string, string)]
+
+proc arr_to_query(arr: dict): string =
+  var query = ""
+  for index, arr in arr.pairs:
+    query = query & arr[0] & "=" & arr[1] & "&"
+  return query
 
 # define requests' methods
-method get(self: Requests, url: string): string {. base .}=
-  return url.getContent()
+method get(
+    self: Requests,
+    url: string,
+    data: dict = {"":""}
+    ) : string {. base .} =
+  # if params are empty
+  if data.len() == 1 and data[0][0] == "" and data[0][1] == "":
+    return url.getContent()
+  else:
+    var full_url = url & "?" & arr_to_query(data)
+    return full_url.getContent()
 
-method post(self: Requests, url: string, data: array[0..0, (string, string)]): string {. base .}=
-  var query = ""
-  for index, arr in data.pairs:
-    query = query & arr[0] & "=" & arr[1] & "&"
-  return url.postContent(body=query)
+method post(
+    self: Requests,
+    url: string,
+    data: dict = {"":""}
+    ) : string {. base .} =
+  # if params are empty
+  if data.len() == 1 and data[0][0] == "" and data[0][1] == "":
+    return url.postContent()
+  else:
+    return url.postContent(body=arr_to_query(data))
 
 var requests: Requests
 
 var a = {"url": "http://google.com/"}
-echo(requests.post("http://xn--yet.tk/api/mk", a))
+echo(
+  requests.post("http://httpbin.org/post", data=a)
+)
