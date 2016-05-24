fcde061178f59463b23bf9180050dca95144b3f2
Emati Mitame <GitCommits@mita.me>
2016-05-24 23:33:36 +0100

    Explicity define the alpha value

--- res/shaders/base.fs
+++ res/shaders/base.fs
@@ -1,12 +1,11 @@
 #version 330 core
 //  in vec3 fragmentColor;
 in vec2 UV;
-out vec3 color;
-uniform sampler2D diffuse;
+out vec4 color;
 
 void main()
 {
 //    color = fragmentColor;
 //    color = texture(diffuse, UV).rgba;
-    color = vec3(1,1,1);
+    color = vec4(1.0, 1.0, 1.0, 1.0);
 }
